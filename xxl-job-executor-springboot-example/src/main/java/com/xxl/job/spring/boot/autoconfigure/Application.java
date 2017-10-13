e91be38e71c2e6ce92a206a269bdb05c3f20fb7f
良帅 <apicescn@hotmail.com>
2017-10-13 19:33:42 +0800

    去掉无须的示例!

--- xxl-job-executor-springboot-example/src/main/java/com/xxl/job/spring/boot/autoconfigure/Application.java
+++ xxl-job-executor-springboot-example/src/main/java/com/xxl/job/spring/boot/autoconfigure/Application.java
@@ -2,10 +2,7 @@ package com.xxl.job.spring.boot.autoconfigure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//import com.xxl.job.core.executor.XxlJobExecutor;
-//import com.xxl.job.spring.boot.autoconfigure.service.jobhandler.DemoJobHandler;
-//
+
 @SpringBootApplication
 public class Application {
 
@@ -13,12 +10,6 @@ public class Application {
 
 		SpringApplication.run(Application.class, args);
 
-//		XxlJobExecutor exe = new XxlJobExecutor("", 9999, "testmain", "http://localhost:8080/", "/data/testmain");
-
-//		XxlJobExecutor.registJobHandler("tttt", new DemoJobHandler());
-//		exe.start();
-
-//		Thread.sleep(1000000000);
 	}
 
 }
